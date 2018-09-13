61b236e7443743ed1abc08075eaae667764b2259
Mario Torres <mario291187@gmail.com>
2018-09-12 21:12:54 -0500

    {withStyles}

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import {withStyles} from '@material-ui/core/styles';
 
+const styles  = theme => {
+
+};
 
 class Footer extends React.Component{
+
     render (){
+        const { classes } = this.props;
         return <Typography>Footer</Typography>
     }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import {withStyles} from '@material-ui/core/styles';
+
+
+const styles = theme => {
+
+};
 
 
 class Header extends React.Component{
 
     render (){
+        const {classes} = this.props;
         return <Typography>Header </Typography>
     }
 }
 
-export default Header;
+export default withStyles(styles)(Header);
