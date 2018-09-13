7d8e7cf78186d2e8549902cffc2e8924e0da5954
Mario Torres <mario291187@gmail.com>
2018-09-12 21:03:04 -0500

    header, footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+
+class Footer extends React.Component{
+    render (){
+        return <Typography>Footer</Typography>
+    }
+}
+
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+
+class Header extends React.Component{
+
+    render (){
+        return <Typography>Header </Typography>
+    }
+}
+
+export default Header;
