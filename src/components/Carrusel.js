dcc10d3e0b0b7aa6312997aa99aa977ef02e7490
Mario Torres <mario291187@gmail.com>
2018-09-19 11:56:08 -0500

    carousel

--- src/components/Carrusel.js
+++ src/components/Carrusel.js
@@ -1,15 +1,69 @@
 import React from 'react';
 import {withStyles} from '@material-ui/core/styles';
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
 
 const styles = theme => ({
     root : {}
 });
 
+
+
+
+function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: "block", background: "#96cb99" }}
+            onClick={onClick}
+        />
+    );
+}
+
+function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ //...style,
+                display: "block", background: "#96cb99" }}
+            onClick={onClick}
+        />
+    );
+}
+
 class Carrusel extends React.Component{
 
     render(){
-        return <div>Carrusel</div>
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        };
+        return (
+            <div>
+                <Slider {...settings}>
+                    <div>
+                        <h3>1</h3>
+                    </div>
+                    <div>
+                        <h3>2</h3>
+                    </div>
+                    <div>
+                        <h3>3</h3>
+                    </div>
+                    <div>
+                        <h3>4</h3>
+                    </div>
+                </Slider>
+            </div>
+        );
     }
 
 }
