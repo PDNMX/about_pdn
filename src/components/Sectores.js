2942a344baf6d96289f24e53c8e50c635bbedc41
Mario Torres <mario291187@gmail.com>
2018-09-27 16:41:48 -0500

    Links

--- src/components/Sectores.js
+++ src/components/Sectores.js
@@ -51,44 +51,54 @@ class Sectores extends React.Component {
                     </Grid>
 
                     <Grid item xs={4} className={ classes.item }>
-                        <img className={classes.icons} src={s1} alt="Sistema 1"/>
-                        <Typography variant="subheading">
-                            <a href="" className={classes.links}> Declaraciones</a>
-                        </Typography>
+                        <a href="http://www.plataformadigitalnacional.org/blog/sistema-1-declaracion-patrimonial-declaracion-de-intereses-y-constancia-de-presentacion-de-declaracion-fiscal/" className={classes.links}>
+                            <img className={classes.icons} src={s1} alt="Sistema 1"/>
+                            <Typography variant="subheading" className={classes.links}>
+                                Declaraciones
+                            </Typography>
+                        </a>
                     </Grid>
                     <Grid item xs={4} className={classes.item}>
-                        <img className={classes.icons} src={s2} alt="Sistema 2"/>
-                        <Typography variant="subheading">
-                            <a href="" className={classes.links}>Servidores públicos que intervienen en contrataciones</a>
-                        </Typography>
+                        <a href="http://www.plataformadigitalnacional.org/blog/sistema-2-servidores-publicos-que-intervienen-en-procesos-de-contrataciones-publicas/" className={classes.links}>
+                            <img className={classes.icons} src={s2} alt="Sistema 2"/>
+                            <Typography variant="subheading" className={classes.links}>
+                                Servidores públicos que intervienen en contrataciones
+                            </Typography>
+                        </a>
                     </Grid>
                     <Grid item xs={4} className = {classes.item}>
-                        <img className={classes.icons} src={s3} alt="Sistema 3"/>
-                        <Typography variant="subheading">
-                            <a href="" className={classes.links}>Servidores públicos y particulares sancionados</a>
-                        </Typography>
+                        <a href="http://www.plataformadigitalnacional.org/blog/sistema-3-servidores-publicos-y-particulares-sancionados/" className={classes.links}>
+                            <img className={classes.icons} src={s3} alt="Sistema 3"/>
+                            <Typography variant="subheading" className={classes.links}>
+                                Servidores públicos y particulares sancionados
+                            </Typography>
+                        </a>
                     </Grid>
                     <Grid item xs={4} className = {classes.item }>
-                        <img className={classes.icons} src={s4} alt="Sistema 4"/>
-                        <Typography variant="subheading">
-                            <a href="" className={classes.links}> Fiscalización</a>
-                        </Typography>
+                        <a href="http://www.plataformadigitalnacional.org/blog/sistema-4-informacion-y-comunicacion-del-sistema-nacional-anticorrupcion-y-el-sistema-nacional-de-fiscalizacion/" className={classes.links}>
+                            <img className={classes.icons} src={s4} alt="Sistema 4"/>
+                            <Typography variant="subheading" className={classes.links}>
+                                Fiscalización
+                            </Typography>
+                        </a>
                     </Grid>
                     <Grid item xs={4} className={classes.item}>
-                        <img className={classes.icons} src={s5} alt="Sistema 5"/>
-                        <Typography variant="subheading">
-                            <a href="" className={classes.links}> Denuncias</a>
-                        </Typography>
+                        <a href="http://www.plataformadigitalnacional.org/blog/sistema-5-denuncias-publicas-de-faltas-administrativas-y-hechos-de-corrupcion/" className={classes.links}>
+                            <img className={classes.icons} src={s5} alt="Sistema 5"/>
+                            <Typography variant="subheading" className={classes.links}>
+                                Denuncias
+                            </Typography>
+                        </a>
                     </Grid>
                     <Grid item xs={4} className={classes.item }>
-                        <img className={classes.icons} src={s6} alt="Sistema 6"/>
-                        <Typography variant="subheading">
-                            <a href="" className={classes.links}> Contrataciones Públicas </a>
-                        </Typography>
+                        <a href="http://www.plataformadigitalnacional.org/blog/sistema-6-informacion-publica-de-contrataciones/" className={classes.links}>
+                            <img className={classes.icons} src={s6} alt="Sistema 6"/>
+                            <Typography variant="subheading" className={classes.links}>
+                                Contrataciones Públicas
+                            </Typography>
+                        </a>
                     </Grid>
-
                 </Grid>
-
             </div>
         )
     }
