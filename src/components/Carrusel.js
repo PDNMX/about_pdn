8b6f7596e0b1088028070f1c0bc2e13a924e650a
Mario Torres <mario291187@gmail.com>
2018-09-27 21:24:47 -0500

    Carrusel

--- src/components/Carrusel.js
+++ src/components/Carrusel.js
@@ -1,103 +1,37 @@
 import React from 'react';
+import SingleLineGridList from './SingleLineGridList';
+import Typography from '@material-ui/core/Typography';
+import {withStyles} from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import IconButton from '@material-ui/core/IconButton';
-import StarBorderIcon from '@material-ui/icons/StarBorder';
-import C1 from '../assets/banner1.png';
-import C2 from '../assets/banner2.png';
-import C3 from '../assets/banner3.png';
-const tileData = [
-    {
-        img: C1,
-        title: 'Infografía 1',
-        author: 'jill111',
-        featured: true,
-    },
-    {
-        img: C2,
-        title: 'Infografía 2',
-        author: 'director90',
-    },
-    {
-        img: C3,
-        title: 'Infografía 3',
-        author: 'Danson67',
-    },
-];
 
 const styles = theme => ({
     root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-        overflow: 'hidden',
-        backgroundColor: theme.palette.background.paper,
-    },
-    gridList: {
-        flexWrap: 'nowrap',
-        // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-        transform: 'translateZ(0)',
+        flexGrow:1
     },
     title: {
-        color: theme.palette.primary.light,
-    },
-    titleBar: {
-        background:
-            'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-    },
+        color: '#96cb99'
+    }
 });
+class Carrusel extends React.Component{
 
-/**
- * The example data is structured as follows:
- *
- * import image from 'path/to/image.jpg';
- * [etc...]
- *
- * const tileData = [
- *   {
- *     img: image,
- *     title: 'Image',
- *     author: 'author',
- *   },
- *   {
- *     [etc...]
- *   },
- * ];
- */
-function SingleLineGridList(props) {
-    const { classes } = props;
+    render(){
 
-    return (
-        <div className={classes.root}>
-            Conoce más
-            <GridList className={classes.gridList} cols={1}>
-                {tileData.map((tile, i) => (
-                    <GridListTile key={i}>
-                        <img src={tile.img} alt={tile.title} />
-                        <GridListTileBar
-                            title={tile.title}
-                            classes={{
-                                root: classes.titleBar,
-                                title: classes.title,
-                            }}
-                            actionIcon={
-                                <IconButton>
-                                    <StarBorderIcon className={classes.title} />
-                                </IconButton>
-                            }
-                        />
-                    </GridListTile>
-                ))}
-            </GridList>
-        </div>
-    );
+        const {classes} = this.props;
+        return (
+            <div className={classes.root}>
+                <Typography variant={'display2'} className={classes.title}>Conoce más</Typography>
+                <br/>
+                <br/>
+                <SingleLineGridList/>
+            </div>
+        )
+    }
+
 }
 
-SingleLineGridList.propTypes = {
-    classes: PropTypes.object.isRequired,
+Carrusel.propTypes = {
+  classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(SingleLineGridList);
+
+export default withStyles(styles)(Carrusel);
