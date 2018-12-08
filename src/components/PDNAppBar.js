f872e630d14169661316e2a8f8726d39c280bb88
Mario Torres <mario291187@gmail.com>
2018-12-08 17:40:06 -0600

    menu

--- src/components/PDNAppBar.js
+++ src/components/PDNAppBar.js
@@ -7,8 +7,12 @@ import PropTypes from 'prop-types';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-//import MenuIcon from '@material-ui/icons/Menu';
 import Grid from '@material-ui/core/Grid';
+import MenuIcon from '@material-ui/icons/Menu';
+//import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import {Link} from 'react-router-dom';
 
 
 const styles = theme => ({
@@ -37,8 +41,31 @@ const styles = theme => ({
 
 class PDNAppBar extends React.Component {
 
+    state = {
+        achorEl: null
+    };
+
+
+
+    handleChange = event => {
+        this.setState({ auth: event.target.checked });
+    };
+
+    handleMenu = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
+    handleClose = () => {
+        this.setState({ anchorEl: null });
+    };
+
+
+
+
     render(){
         const {classes } = this.props;
+        const { auth, anchorEl } = this.state;
+        const open = Boolean(anchorEl);
 
         return (
             <div className={classes.root}>
@@ -57,9 +84,35 @@ class PDNAppBar extends React.Component {
                                 {/*<Button color="inherit" href="/datos">
                             Datos
                         </Button>*/}
-                                <Button color="inherit" href="/blog" className={classes.lastButton}>
-                                    Blog
-                                </Button>
+
+
+                                <div>
+                                    <IconButton
+                                        aria-owns={open ? 'menu-appbar' : undefined}
+                                        aria-haspopup="true"
+                                        onClick={this.handleMenu}
+                                        color="inherit"
+                                    >
+                                        <MenuIcon/>
+                                    </IconButton>
+                                    <Menu
+                                        id="menu-appbar"
+                                        anchorEl={anchorEl}
+                                        anchorOrigin={{
+                                            vertical: 'top',
+                                            horizontal: 'right',
+                                        }}
+                                        transformOrigin={{
+                                            vertical: 'top',
+                                            horizontal: 'right',
+                                        }}
+                                        open={open}
+                                        onClose={this.handleClose}
+                                    >
+                                        <MenuItem onClick={this.handleClose} onClick={() => { window.location.href = "https://www.plataformadigitalnacional.org/blog/"}}>Blog</MenuItem>
+                                        <MenuItem onClick={this.handleClose} component={Link} to="/terminos">Términos de uso</MenuItem>
+                                    </Menu>
+                                </div>
                             </Toolbar>
                         </Grid>
                     </Grid>
